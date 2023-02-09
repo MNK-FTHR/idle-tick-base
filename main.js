f2806f719c315b55ec0b7e7bdbcfc9b5ee9cf43d
MNK-FTHR <victorgranier.pierre@gmail.com>
2023-02-09 10:53:56 +0100

    base
    here is the base of the work, thanks to https://gist.github.com/HipHopHuman/3e9b4a94b30ac9387d9a99ef2d29eb1a

---
+++ main.js
@@ -0,0 +1,19 @@
+const loop = new GameLoop();
+
+let currency = 0;
+let currency_per_millisecond = 0.003;
+
+loop.onUpdate = function(dt, t) {
+  currency += currency_per_millisecond * dt;
+};
+
+loop.onRender = function(i) {
+  currencyEl.textContent = currency.toFixed(2);
+};
+
+loop.onPanic = function() {
+  // discard any accumulated lag time and hope for the best
+  this.timing.lag = 0;
+};
+
+loop.start();
